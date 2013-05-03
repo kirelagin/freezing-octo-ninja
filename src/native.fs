7828436f94439308e7a88db4256973261f288ed2
Kirill Elagin <kirelagin@gmail.com>
2013-05-03 18:40:53 +0400

    Fix signature generator

--- src/native.fs
+++ src/native.fs
@@ -9,6 +9,8 @@ module Native =
     let registerNativeMethod (m, n) = Dalvik.ThreadWorker.nativelib.[m] <- n
 
     let genSignature (cls : string, name : string, ret : string, args : string array) : Dex.Method =
-        let shorty = String.concat "" <| Array.map (fun (s : string) -> s.Substring (0, 1)) (Array.append [| ret |] args)
+        let shorty = String.concat "" <| Array.map (fun (s : string) ->
+                                                        let c = s.Substring (0, 1)
+                                                        if c = "[" then "L" else c) (Array.append [| ret |] args)
         let proto = Dex.Proto (shorty, Dex.Type ret, Array.map Dex.Type args)
         Dex.Method (Dex.Type cls, proto, name)
