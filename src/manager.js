69f9378a779f3566e084e0c99eb9db312449e8e8
Kirill Elagin <kirelagin@gmail.com>
2013-05-24 23:43:50 +0400

    Restore multiple-dex loading

--- src/manager.js
+++ src/manager.js
@@ -13,25 +13,29 @@ IntelliFactory.Runtime.Define(Dalvik, {
 
 
 IntelliFactory.Runtime.Start();
+Dalvik.Native.init();
 
 var interactive = self;
 // First message is from the interactivity manager
 self.onmessage = function(e) {
-    self.onmessage = undefined;
-    Dalvik.Manager.init()(e.data);
+    tag = e.data[0];
+    d = e.data[1];
 
-    Dalvik.Native.init();
-    r = Dalvik.Native.genSignature("LMain;", "main", "V", ["[Ljava/lang/String;"])
-    c = Dalvik.Manager.library().get_Item(r.$0)
-    impl = Dalvik.Runtime.getMethodImpl(c.$0, true, r)
+    if (tag == 'load') {
+        Dalvik.Manager.init()(d);
+    } else {
+        r = Dalvik.Native.genSignature("LMain;", "main", "V", ["[Ljava/lang/String;"])
+        c = Dalvik.Manager.library().get_Item(r.$0)
+        impl = Dalvik.Runtime.getMethodImpl(c.$0, true, r)
 
-    chan = new MessageChannel()
+        chan = new MessageChannel()
 
-    chan.port1.onmessage = function(e) {
-        Dalvik.Manager.processRequest(e.data, function(reply) {
-            chan.port1.postMessage(reply);
-        });
+        chan.port1.onmessage = function(e) {
+            Dalvik.Manager.processRequest(e.data, function(reply) {
+                chan.port1.postMessage(reply);
+            });
+        };
+
+        self.postMessage(['spawn', [r.$0, r, impl.$0]], [chan.port2])
     };
-
-    self.postMessage(['spawn', [r.$0, r, impl.$0]], [chan.port2])
 };
